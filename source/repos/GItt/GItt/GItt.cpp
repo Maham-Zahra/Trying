5c02e074bdb7cbbc20cd8d334dc06c952cfa911a
Maham Zahra <mahamzahra154@gmail.com>
2024-12-16 11:55:02 +0500

    Sum of two numbers

---
+++ source/repos/GItt/GItt/GItt.cpp
@@ -0,0 +1,11 @@
+// GItt.cpp :
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b,sum=0;
+	cout << "Enter two numbers  ";
+	cin >> a >> b;
+	sum = a + b;
+	cout << "Sum= " << sum;
+}
